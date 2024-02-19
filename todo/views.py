e69fe57fa1760a9e917c3d558592853f176d8811
Rushil Khatri <rushilkhatri1836@gmail.com>
2024-02-19 18:48:35 +0530

    add tasks functionalities

--- todo/views.py
+++ todo/views.py
@@ -19,3 +19,22 @@ def mark_as_undone(request,pk):
     tassk_n.is_completed=False
     tassk_n.save()
     return redirect('home')
+
+def edit_task(request, pk):
+    edit_t= get_object_or_404(task, pk=pk)
+    if request.method == 'POST':
+        updated = request.POST['task']
+        edit_t.task = updated
+        edit_t.save()
+        return redirect('home')
+    else:
+        context={
+            'edit_t':edit_t
+        }
+
+        return render(request, 'edit.html', context)
+    
+def delete_task(request, pk):
+    dele= get_object_or_404(task,pk=pk )
+    dele.delete()
+    return redirect('home')
